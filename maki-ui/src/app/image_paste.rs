054d94894624d882fdb1dcfbb9bae4abd6782ff4
w0wl0lxd <w0wl0lxd@tuta.com>
2026-07-09 00:34:25 +0300

    ui: Don't accept image input on models that don't support images

--- maki-ui/src/app/image_paste.rs
+++ maki-ui/src/app/image_paste.rs
@@ -6,13 +6,23 @@ use maki_agent::{ImageMediaType, ImageSource};
 
 use super::App;
 
+const IMAGE_NOT_SUPPORTED_MSG: &str = "Model does not support image input";
+
 impl App {
     pub(super) fn start_file_image_paste(&mut self, path: PathBuf, media_type: ImageMediaType) {
+        if !self.state.model.vision {
+            self.status_bar.flash(IMAGE_NOT_SUPPORTED_MSG.into());
+            return;
+        }
         let msg = format!("Reading {}...", path.display());
         self.spawn_image_load(msg, move || image::load_file_image(&path, media_type));
     }
 
     pub(super) fn start_image_paste(&mut self) {
+        if !self.state.model.vision {
+            self.status_bar.flash(IMAGE_NOT_SUPPORTED_MSG.into());
+            return;
+        }
         self.spawn_image_load("Reading clipboard...".into(), image::load_clipboard_image);
     }
 
@@ -39,8 +49,12 @@ impl App {
             self.image_paste_rx.swap_remove(i);
             match result {
                 Ok(source) => {
-                    self.input_box.attach_image(source);
-                    self.status_bar.flash("Image attached".into());
+                    if !self.state.model.vision {
+                        self.status_bar.flash(IMAGE_NOT_SUPPORTED_MSG.into());
+                    } else {
+                        self.input_box.attach_image(source);
+                        self.status_bar.flash("Image attached".into());
+                    }
                 }
                 Err(e) => self.status_bar.flash(format!("Image paste failed: {e}")),
             }
