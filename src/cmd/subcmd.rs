6a932212bcb18e40c85cf61ebda7035856086b4a
w0wl0lxd <w0wl0lxd@tuta.com>
2026-07-19 00:54:11 -0400

    style(subcmd): drop unused eyre import
    Signed-off-by: w0wl0lxd <w0wl0lxd@tuta.com>

--- src/cmd/subcmd.rs
+++ src/cmd/subcmd.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use std::sync::Arc;
 
 use color_eyre::Result;
-use color_eyre::eyre::{Context, bail, eyre};
+use color_eyre::eyre::{Context, bail};
 
 use maki_agent::mcp::{config as mcp_config, oauth as mcp_oauth};
 use maki_agent::tools::ToolRegistry;
