e9cbeb3f3ff5538bfd704400e649e557216aac53
github-action-benchmark <github@users.noreply.github.com>
2026-07-22 07:52:13 +0000

    add Criterion (cargo) benchmark result for 03405365955f042ffda46d64ff19290c0a1a9436

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784705376773,
+  "lastUpdate": 1784706733482,
   "repoUrl": "https://github.com/w0wl0lxd/n00n",
   "entries": {
     "Criterion": [
@@ -107,6 +107,114 @@ window.BENCHMARK_DATA = {
             "name": "splash_render_200x60",
             "value": 102008,
             "range": "± 13695",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "w0wl0lxd@tuta.com",
+            "name": "w0wl0lxd",
+            "username": "w0wl0lxd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03405365955f042ffda46d64ff19290c0a1a9436",
+          "message": "Merge pull request #62 from w0wl0lxd/fix/explore-card-jank",
+          "timestamp": "2026-07-22T03:38:37-04:00",
+          "tree_id": "0bfde7159b80eab2cc0c5b2eaf88b4801d634c62",
+          "url": "https://github.com/w0wl0lxd/n00n/commit/03405365955f042ffda46d64ff19290c0a1a9436"
+        },
+        "date": 1784706733129,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/jit_mlua_hook",
+            "value": 6602745,
+            "range": "± 284819",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/jit_watchdog",
+            "value": 2341217,
+            "range": "± 4518",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/jit_none",
+            "value": 2352487,
+            "range": "± 15448",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/interp_mlua_hook",
+            "value": 9453060,
+            "range": "± 91854",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/interp_watchdog",
+            "value": 4293218,
+            "range": "± 12639",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/interp_none",
+            "value": 4363725,
+            "range": "± 31111",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "buffer_rw/jit_mlua_hook",
+            "value": 584221,
+            "range": "± 1353",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "buffer_rw/jit_watchdog",
+            "value": 191982,
+            "range": "± 296",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "buffer_rw/jit_none",
+            "value": 191852,
+            "range": "± 2245",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "buffer_rw/interp_mlua_hook",
+            "value": 1030571,
+            "range": "± 9687",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "buffer_rw/interp_watchdog",
+            "value": 564254,
+            "range": "± 2431",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "buffer_rw/interp_none",
+            "value": 563436,
+            "range": "± 13341",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "splash_render_120x40",
+            "value": 49574,
+            "range": "± 2047",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "splash_render_200x60",
+            "value": 191244,
+            "range": "± 1633",
             "unit": "ns/iter"
           }
         ]
