fe92c2d18afa21f1018070882c46049488ae1a86
w0wl0lxd <w0wl0lxd@tuta.com>
2026-07-26 19:39:23 -0400

    fix(nix): embed runtime library paths

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
               packageName
             ];
             nativeBuildInputs = with pkgs; [
-              makeWrapper
+              patchelf
               pkg-config
               perl
               python3
@@ -91,12 +91,16 @@
               fi
               done
             '';
-            buildInputs = with pkgs; [ openssl stdenv.cc.cc.lib ];
+            buildInputs = with pkgs; [
+              openssl
+              stdenv.cc.cc.lib
+            ];
             doCheck = false;
 
             postInstall = ''
-              wrapProgram $out/bin/n00n \
-                --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath runtimeLibs}"
+              old_rpath="$(patchelf --print-rpath "$out/bin/${packageName}")"
+              new_rpath="${lib.makeLibraryPath runtimeLibs}''${old_rpath:+:$old_rpath}"
+              patchelf --set-rpath "$new_rpath" "$out/bin/${packageName}"
             '';
           };
         in
